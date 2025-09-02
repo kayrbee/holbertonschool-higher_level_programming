6148b4c81e46d50dda859e3085dd7aa055dd4d01
Katherine Beringer <github@katbee.au>
2025-09-02 10:42:59 +1000

    If/elif/else

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number < 0:
+    result = 'negative'
+elif number == 0:
+    result = 'zero'
+else:
+    result = 'positive'
+# Qu: when are the curly braces required inside a print statement?
+print(number, ' is ', result)
