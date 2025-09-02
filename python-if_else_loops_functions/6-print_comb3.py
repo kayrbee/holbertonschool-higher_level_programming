087e330f4683f1555ffec933532885c5a8b0e3d0
Katherine Beringer <github@katbee.au>
2025-09-03 09:43:36 +1000

    Print combinations of numbers in nested loop

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for i in range(0, 10):
+    for j in range(1, 10):
+        if j > i and not i == 8:
+            print("{}{}".format(i, j), end=', ')
+print("{}{}".format(i - 1, j))
