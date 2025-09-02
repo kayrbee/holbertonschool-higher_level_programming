76381a95a5b90d631855d149d5ff33442fce36be
Katherine Beringer <github@katbee.au>
2025-09-02 15:36:27 +1000

    Print all except

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_digit = (number * -1) % 10
-    last_digit = last_digit * -1
+    last_digit = ((number * -1) % 10) * -1
+    # last_digit = last_digit * -1
 else:
     last_digit = number % 10
 if last_digit > 5:
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for i in range(97, 123):
+    if not (i == 113) and not (i == 101):
+        print("{}".format(chr(i)), end='')
