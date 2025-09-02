da588129fe5f6d27be4c0e393b018d5128206ebe
Katherine Beringer <github@katbee.au>
2025-09-03 09:48:53 +1000

    Refactor separator handling

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-
+first = True
 for i in range(0, 10):
     for j in range(1, 10):
-        if j > i and not i == 8:
-            print("{}{}".format(i, j), end=', ')
-print("{}{}".format(i - 1, j))
+        if j > i:
+            if not first:
+                print(", ", end='')
+            print("{}{}".format(i, j), end='')
+            first = False
+print()
