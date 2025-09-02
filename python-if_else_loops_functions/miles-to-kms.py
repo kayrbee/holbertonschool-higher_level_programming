291cc89f43eae7ec61339bd8aa02337381a71261
Katherine Beringer <github@katbee.au>
2025-09-02 13:02:30 +1000

    Refactor to use .format

---
+++ python-if_else_loops_functions/miles-to-kms.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+miles = input("Enter miles to convert to kms: ")
+miles = int(miles)
+kilometres = miles * 1.6093
+
+print("{} miles equals {} kilmetres".format(miles, kilometres))
