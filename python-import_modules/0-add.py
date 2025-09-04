d76f95b5f9b3c0112eccd7beb1329b090605f4e1
Katherine Beringer <github@katbee.au>
2025-09-04 11:10:27 +1000

    import a module with an alias

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# add_module is an internal alias for human-friendly use
+import add_0 as add_module
+
+a = 1
+b = 2
+c = add_module.add(a, b)
+print("{} + {} = {}".format(a, b, c))
