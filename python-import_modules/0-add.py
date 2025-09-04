67118614916ac6a8a950c2c5a283c8923b9cf3fe
Katherine Beringer <github@katbee.au>
2025-09-04 11:20:28 +1000

    Add main check to main file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# add_module is an internal alias for human-friendly use
-import add_0 as add_module
+if __name__ == "__main__":
+    # add_module is an internal alias for human-friendly use
+    import add_0 as add_module
 
-a = 1
-b = 2
-c = add_module.add(a, b)
-print("{} + {} = {}".format(a, b, c))
+    a = 1
+    b = 2
+    c = add_module.add(a, b)
+    print("{} + {} = {}".format(a, b, c))
+    
