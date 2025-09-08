fda3ac82dca84ac13cf42790e221f63a096e0486
Katherine Beringer <github@katbee.au>
2025-09-08 16:15:36 +1000

    Iterate by each element

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    i = 0
+    rows, cols = 0, 0
+    size = len(matrix)
+    columns = len(matrix[0])
     if matrix == [[]]:
         return print()
-    while (i < len(matrix)):
-        print("{}".format(matrix[i]))
-        i += 1
+    while (rows < size):
+        while (cols < columns):
+            print("{:d} ".format(matrix[rows][cols]), end='')
+            cols += 1
+        print()
+        rows += 1
+        cols = 0
 
-# # Tests
+# Tests
 # matrix = [
 #     [1, 2, 3],
 #     [4, 5, 6],
 #     [7, 8, 9]
+# ]
+# matrix = [
+#     [1, 2],
+#     [4, 5],
+#     [7, 8]
 # ]
 
 # print_matrix_integer(matrix)
