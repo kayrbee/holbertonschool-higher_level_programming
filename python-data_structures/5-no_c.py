c0eaabc1021396960c2ae7df2af407a0c6cea1cb
Katherine Beringer <github@katbee.au>
2025-09-08 15:32:01 +1000

    Try other solutions

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
-    for char in my_string:
-        if (char == 'C') or (char == 'c'):
-            continue
-        else:
-            new_string += char
-    return new_string
+    new_string = my_string[:]
+    return new_string.replace('c', '').replace('C', '')
 
 # Alternative approaches:
 # #  Use join
 # def no_c(my_string):
 #     return ''.join(ch for ch in my_string if ch not in 'cC')
 
-# #  Use replace
+# #  My original version
 # def no_c(my_string):
-#     return my_string.replace('c', '').replace('C', '')
+    # for char in my_string:
+    #     if (char == 'C') or (char == 'c'):
+    #         continue
+    #     else:
+    #         new_string += char
+    # return new_string
 
 # #  Use filter
 # def no_c(my_string):
