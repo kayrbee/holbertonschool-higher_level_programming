89bd014458972fbd2fca39f46f1b7025e297d0dd
Katherine Beringer <github@katbee.au>
2025-09-08 15:37:07 +1000

    Try join instead of replace

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    new_string = my_string[:]
-    return new_string.replace('c', '').replace('C', '')
+# def no_c(my_string):
+#     for char in my_string:
+#         if (char == 'C') or (char == 'c'):
+#             continue
+#         else:
+#             new_string += char
+#     return new_string
 
 # Alternative approaches:
 # #  Use join
-# def no_c(my_string):
-#     return ''.join(ch for ch in my_string if ch not in 'cC')
+def no_c(my_string):
+    new_string = my_string[:]
+    return ''.join(ch for ch in new_string if ch not in 'cC')
 
-# #  My original version
-# def no_c(my_string):
-    # for char in my_string:
-    #     if (char == 'C') or (char == 'c'):
-    #         continue
-    #     else:
-    #         new_string += char
-    # return new_string
+# #  Use replace (disallowed in this exercise)
 
 # #  Use filter
 # def no_c(my_string):
 #     return ''.join(filter(lambda ch: ch not in 'cC', my_string))
 
-
-# Tests
+# # Tests
 # print(no_c("Best School"))
 # print(no_c("Chicago"))
 # print(no_c("C is fun!"))
