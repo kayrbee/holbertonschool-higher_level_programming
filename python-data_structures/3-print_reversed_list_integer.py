db44f1050b0e8b0b3e2ab9a96a1f39185f541dd8
Katherine Beringer <github@katbee.au>
2025-09-08 14:31:44 +1000

    Alternative approaches

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,3 +4,12 @@ def print_reversed_list_integer(my_list=[]):
     while i > 0:
         print("{:d}".format(my_list[i - 1]))
         i -= 1
+
+# Alternative approaches:
+    # Use list slicing
+    # for num in my_list[::-1]:
+    #     print("{:d}".format(num))
+    # 
+    # Use reversed() in-built method
+    # for num in reversed(my_list):
+    # print("{:d}".format(num))
