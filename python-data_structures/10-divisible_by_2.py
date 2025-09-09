ccb13e2d5018521cc0349908c5651b87c5d69675
Katherine Beringer <github@katbee.au>
2025-09-09 11:47:32 +1000

    Refactor loop logic to one line

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,16 +3,10 @@ def divisible_by_2(my_list=[]):
     list_copy = my_list[:]
     list_result = []
     for item in list_copy:
-        if (item % 2 == 0):
-            result = 1
-        else:
-            result = 0
-        list_result.append(result)
+        list_result.append(0 if item % 2 else 1)
     return list_result
 
-# Alternative approaches:
-
-# # Tests
+# # # Tests
 # my_list = [0, 1, 2, 3, 4, 5, 6]
 # list_result = divisible_by_2(my_list)
 
