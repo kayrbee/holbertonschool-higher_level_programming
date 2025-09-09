c28f056881a7d790c5e354ab7b88b6082729a7e7
Katherine Beringer <github@katbee.au>
2025-09-09 16:09:15 +1000

    Count keys in dict

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    return len(a_dictionary)
+# Alternative:
+
+# # Tests
+# # a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+# a_dictionary = { }
+# nb_keys = number_keys(a_dictionary)
+# print("Number of keys: {:d}".format(nb_keys))
