691f66f8e3729ba42672731d543a8f4fbf4d3158
Katherine Beringer <github@katbee.au>
2025-09-09 14:23:34 +1000

    Find and replace in a list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_matrix = []
+    for i in my_list:
+        if (i == search):
+            new_matrix.append(replace)
+        else:
+            new_matrix.append(i)
+    return new_matrix
+
+# Tests
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
