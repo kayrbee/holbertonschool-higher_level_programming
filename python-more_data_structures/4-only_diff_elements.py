a9a57fbf0553d75c53c40d8bcfb7391e4e971967
Katherine Beringer <github@katbee.au>
2025-09-09 16:05:21 +1000

    Refactor to use symmetric_diff

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_1_diff = set_1.difference(set_2)
-    set_2_diff = set_2.difference(set_1)
-    set_c = set_2_diff.union(set_1_diff)
+    set_c = set_1.symmetric_difference(set_2)
     return set_c
 # Alternative:
 
-# Tests
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
+# # Tests
+# set_1 = { "Python", "C", "Javascript" }
+# set_2 = { "Bash", "C", "Ruby", "Perl" }
+# od_set = only_diff_elements(set_1, set_2)
+# print(sorted(list(od_set)))
