48f29846d57257da0ead25f8c738867e4828443e
Katherine Beringer <github@katbee.au>
2025-09-09 16:24:29 +1000

    Fix formatting

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary.keys()):
-        print(i, ": ", a_dictionary[i])
+        print("{}: {}".format(i, a_dictionary[i]))
 # Alternative:
 
 # # Tests
-# a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+# a_dictionary = { 'language': "C", 'Number': 89, \
+#   'track': "Low level", 'ids': [1, 2, 3] }
 # print_sorted_dictionary(a_dictionary)
