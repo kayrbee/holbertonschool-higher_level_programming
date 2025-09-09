2315f9716546aea0b28349c2d957b83da9353046
Katherine Beringer <github@katbee.au>
2025-09-09 16:44:56 +1000

    Pycodestyle fixes

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
