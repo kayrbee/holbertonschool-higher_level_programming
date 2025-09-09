2b7b3ea1ce73c7a4928d043a9be576fee2185e8c
Katherine Beringer <github@katbee.au>
2025-09-09 14:27:49 +1000

    Refactor loop logic to one line

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,15 +2,12 @@
 def search_replace(my_list, search, replace):
     new_matrix = []
     for i in my_list:
-        if (i == search):
-            new_matrix.append(replace)
-        else:
-            new_matrix.append(i)
+        new_matrix.append(replace if i == search else i)
     return new_matrix
 
-# Tests
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
+# # Tests
+# my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+# new_list = search_replace(my_list, 2, 89)
 
-print(new_list)
-print(my_list)
+# print(new_list)
+# print(my_list)
