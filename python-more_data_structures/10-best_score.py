b92590e5ad52d0d40a349e608ece9ab3ee2d2976
Katherine Beringer <github@katbee.au>
2025-09-10 11:19:01 +1000

    Fix/handle empty dict

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or a_dictionary == {}:
         return "None"
     best = max(a_dictionary, key=a_dictionary.get)
     return best
