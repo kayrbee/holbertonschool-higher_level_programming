b3f612d8e04b6f82e12f2ee560ec868fdf8aada8
Katherine Beringer <github@katbee.au>
2025-09-10 18:31:15 +1000

    Version using enumerate

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    total, prev_int = 0, 0
+    total, prev_int, i = 0, 0, 0
     roman_dict = {
         'I': 1,
         'V': 5,
@@ -13,65 +13,69 @@ def roman_to_int(roman_string):
         'M': 1000
         }
 
-    for ch in roman_string:
+    for index, ch in enumerate(roman_string):
         current_int = roman_dict.get(ch, 0)
-        if current_int > prev_int:
-            total += current_int - (2 * prev_int)
-        else:
+        if index < (len(roman_string) -1):
+            next_int = roman_dict.get(roman_string[index + 1], 0)
+            if current_int < next_int:
+                total -= current_int
+            else:
                 total += current_int
-        prev_int = current_int
+        else:
+            total += current_int
+        
     return total
 
 # # Tests
-# # 10
-# roman_number = "X"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 10
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 7
-# roman_number = "VII"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 7
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# roman_number = None
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = None
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# roman_number = 2
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = 2
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# roman_number = ""
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = ""
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 4
-# roman_number = "IV"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 4
+roman_number = "IV"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 6
-# roman_number = "VI"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 6
+roman_number = "VI"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 9
-# roman_number = "IX"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 9
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 87
-# roman_number = "LXXXVII"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 87
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 707
-# roman_number = "DCCVII"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 707
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 124
-# roman_number = "CXXIV"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 124
+roman_number = "CXXIV"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 99
-# roman_number = "XCIX"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 99
+roman_number = "XCIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 89
-# roman_number = "LXXXIX"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 89
+roman_number = "LXXXIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# # 1900
-# roman_number = "MCM"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+# 1900
+roman_number = "MCM"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
