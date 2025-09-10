7c09f4e87e7f69586297d84b036c7522d05a7d05
Katherine Beringer <github@katbee.au>
2025-09-10 12:39:55 +1000

    Get IX to pass

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,9 +8,14 @@ def roman_to_int(roman_string):
     roman_dict = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
     length = len(roman_string)
     for i in range(length):
-        integer_equivalent += roman_dict.get(roman_string[i])
-        # print("l: {}".format(length))
-        # print("{}".format(roman_dict[roman_string[i]]))
+        # integer_equivalent += roman_dict.get(roman_string[i])
+        current_int = roman_dict.get(roman_string[i])
+        # if roman_string[i] == 'I' and roman_string[i + 1] == 'X':
+        if roman_string[i] == 'I':
+            integer_equivalent = roman_dict.get(roman_string[i + 1]) - current_int
+            break
+        else:
+            integer_equivalent += roman_dict.get(roman_string[i])
 
     return integer_equivalent
 
@@ -21,8 +26,8 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 roman_number = "VII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# roman_number = "IX"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 # roman_number = "LXXXVII"
 # print("{} = {}".format(roman_number, roman_to_int(roman_number)))
