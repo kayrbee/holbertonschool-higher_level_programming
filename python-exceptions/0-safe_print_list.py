3486fdfe5d86fde48bea2551d43527e331f72983
Katherine Beringer <github@katbee.au>
2025-09-11 10:52:43 +1000

    Remove extra spaces from print()

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for index, item in enumerate(my_list):
         if index < x:
             try:
-                print("item: {} ".format(item), end='')
+                print("{}".format(item), end='')
                 counter += 1
             except IndexError:
                 print("x larger than length my_list")
