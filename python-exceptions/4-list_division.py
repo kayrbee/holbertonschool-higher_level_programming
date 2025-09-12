6efb4fa1606edd9f1140e23ddc9e26cbe46e36bc
Katherine Beringer <github@katbee.au>
2025-09-12 16:56:20 +1000

    Fix/Add continue to IndexError

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,7 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
             continue
         except IndexError:
             print("out of range")
-            break
+            continue
         # finally:
 
     return result_list
