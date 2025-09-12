09991ee809e6030a51bf4d27790d86a6efa654b6
Katherine Beringer <github@katbee.au>
2025-09-12 17:01:08 +1000

    Refactor to use finally: block

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,10 +4,9 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(len(result_list)):
         try:
-            result_list[i] = my_list_1[i] / my_list_2[i]
-            # continue
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            result_list[i] = 0
+            result = 0
             print("division by 0")
             continue
         except TypeError:
@@ -16,11 +15,12 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             continue
-        # finally:
+        finally:
+            result_list[i] = result
 
     return result_list
 
-# Tests
+# # Tests
 # my_l_1 = [10, 8, 4]
 # my_l_2 = [2, 4, 4]
 # result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
