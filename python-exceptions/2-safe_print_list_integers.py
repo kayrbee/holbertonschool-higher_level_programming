e4b46f5288c0d419ea95c5041fd1e3164f291c6e
Katherine Beringer <github@katbee.au>
2025-09-12 16:06:52 +1000

    Fix/len() disallowed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
+    length_li = 0
+    for item in my_list:
+        length_li += 1
     for index, item in enumerate(my_list):
-        if x > len(my_list):
-            raise IndexError
         if index < x:
             try:
                 print("{:d}".format(item), end='')
                 counter += 1
             except TypeError:
-                # print("TypeError")
                 continue
             except ValueError:
-                # print("ValueError")
                 continue
+        if x > length_li:
+            raise IndexError
+        
+
+            
     print()
     return counter
 
-# # Tests
-# my_list = [1, 2, 3, 4, 5]
+# Tests
+my_list = [1, 2, 3, 4, 5]
 
-# nb_print = safe_print_list_integers(my_list, 2)
-# print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
 
-# my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-# nb_print = safe_print_list_integers(my_list, len(my_list))
-# print("nb_print: {:d}".format(nb_print))
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
 
-# nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-# print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
