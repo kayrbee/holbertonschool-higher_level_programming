8e4b8fd053bedc869fda76797cd99a75fa8965ea
Katherine Beringer <github@katbee.au>
2025-09-13 17:00:23 +1000

    Fix/pycodestyle line breaks

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,7 +12,7 @@ def matrix_divided(matrix, div):
     Args:
       matrix (list of lists) : integers/floats
       div : integer demoninator
-    
+
     Returns:
       new matrix containing the results,
       rounded to 2 decimal places
@@ -22,7 +22,7 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if len(row) != len(matrix[0]):
             row_size_check = 1
-   
+
     if row_size_check == 1:
         raise TypeError("Each row of the matrix must have the same size")
 
@@ -35,11 +35,10 @@ def matrix_divided(matrix, div):
     for row in matrix:
         for i in row:
             if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-    result = [[round(matrix[i][j] / div, 2) for j in range(len(matrix[0]))] for i in range(len(matrix))]
-    return result
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of"
+                    " integers/floats")
 
-# matrix = [[1, 2, 3], ["s", "p", "c"]]
-# res = matrix_divided(matrix, 3)
-# print(res)
+    result = [[round(matrix[i][j] / div, 2) for j in range(len(matrix[0]))]
+              for i in range(len(matrix))]
+    return result
