4074178107fa5f2cb33fa2aadfcb5675f70b51db
Katherine Beringer <github@katbee.au>
2025-09-13 15:24:51 +1000

    m_divide i0

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains one function for
+dividing all elements in a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Takes a matrix and a division denominator
+
+    Args:
+      matrix (list of lists) : integers/floats
+      div : integer demoninator
+    
+    Returns:
+      new matrix containing the results,
+      rounded to 2 decimal places
+    """
+    new_matrix = [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+
+    return new_matrix
