418cf32abcbb3ddd0cf16fb5de59c24763f7a115
Katherine Beringer <github@katbee.au>
2025-09-13 15:08:46 +1000

    Fix/pycodestyle errors

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 """
 This module contains one function for adding 2 integers together
 """
+
+
 def add_integer(a, b=98):
     """
     Sums up the arguments passed in, using 98 as a default value of b
@@ -19,18 +21,7 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
 
-    
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-
-# >>> add_integer(3, -2)
-# -1
-
-# >>> add_integer(2, b)
-# 100
-# >>> add_integer(1, 'School')
-# 'b must be an integer'
-
-# >>> add_integer(None)
-# 'a must be an integer'
