7c1f2ae735152fb0ecb0a328a31d3ef0df7ca433
Katherine Beringer <github@katbee.au>
2025-09-14 13:13:13 +1000

    Fix/pycodestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,7 +15,7 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError('text must be a string')
-    
+
     markers = ['.', '?', ':', '*** ']
     text_copy = text[:]
 
@@ -49,12 +49,14 @@ def text_indentation(text):
 # Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
 # Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
 # Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-# rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+# rationi oboediens. Si id dicis, vicimus. \
+# Inde sermone vario sex illa a Dipylo \
 # stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
 # cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
 # beatiorem! Iam ruinas videres"""
 # text = 'Holberton School'
 # text_indentation(text)
-# text_indentation("First sentence ends with period. Second sentence doesn't end with a marker")
+# text_indentation("First sentence ends with period. \
+# Second sentence doesn't end with a marker")
 # text_indentation("One.School")
 # text_indentation("One.School.")
