c4f33b78aa8e34b5a07ed3ac5ba487757a5e97d5
Katherine Beringer <github@katbee.au>
2025-09-14 10:22:43 +1000

    Fix/pycodestyle errors

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,7 @@ This module contains one function for printing str arguments
 
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Print
@@ -13,19 +14,18 @@ def say_my_name(first_name, last_name=""):
       first_name : string containing first name
       last_name : optional string containing last name
         defaults to ""
-    
+
     Returns:
       Nothing
     """
 
     if (type(first_name) is not str):
         raise TypeError("first_name must be a string")
-    
-    if(type(last_name) is not str):
-        raise TypeError("last_name must be a string")
-    
-    print("My name is {} {}".format(first_name, last_name))
 
+    if (type(last_name) is not str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
 
     if __name__ == "__main__":
         import doctest
