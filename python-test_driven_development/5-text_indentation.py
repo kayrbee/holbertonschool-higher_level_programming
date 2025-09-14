22149bb6ff8396ebf79a3a2c1e61460e5c12a648
Katherine Beringer <github@katbee.au>
2025-09-14 12:00:40 +1000

    mvp handle . ? : logic

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,14 +16,39 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
 
-    chunk = [sentence for sentence in text.split(". ")]
-    last = len(chunk)
+    #  handle periods  
+    p_chunk = [sentence for sentence in text.split(".")]
+    p = ".***".join(p_chunk)
+    # print(p)
 
-    for pos, ch in enumerate(chunk):
-        if pos != last - 1:
-            print("{}".format(ch), end='.\n\n')
-        else:
-            print("{}".format(ch), end='\n\n')
+    #   handle question marks
+    q_chunk = [sentence for sentence in p.split("?")]
+    q = "?***".join(q_chunk)
+    # print(q)
 
-# text = "string to print. second string?"
-# text_indentation(text)
+    #   handle colons
+    c_chunk = [sentence for sentence in q.split(":")]
+    c = ":***".join(c_chunk)
+    # print(c)
+
+    #  strip spaces at start of sentences
+    w_parse = [word for word in c.split('* ')]
+    print("w_parse: ", w_parse)
+    w = "".join(w_parse)
+    print("w: ", w)
+
+    chunk = [sentence for sentence in w.split("**")]
+    for ch in chunk:
+        print("{}".format(ch), end='\n\n')
+    # print(c)
+
+    # last = len(chunk)
+
+    # for pos, ch in enumerate(p_chunk):
+    #     if pos != last - 1:
+    #         print("{}".format(ch.split('**')), end='.\n\n')
+    #     else:
+    #         print("{}".format(ch), end='\n\n')
+
+text = "string to print. second string? third string!"
+text_indentation(text)
