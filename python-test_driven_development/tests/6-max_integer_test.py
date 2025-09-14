ac0409cc2b71e4bd29307a6f6dc56045f86b85a1
Katherine Beringer <github@katbee.au>
2025-09-14 13:34:40 +1000

    Add max at beginning test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,6 +7,9 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max_at_the_end(self):
         self.assertEquals(max_integer([1, 2, 3, 4]), 4)
+    
+    def test_max_at_the_beginning(self):
+        self.assertEquals(max_integer([1, 2, 3, 4]), 1)
 
     def test_empty_list(self):
         self.assertIsNone(max_integer([]))
