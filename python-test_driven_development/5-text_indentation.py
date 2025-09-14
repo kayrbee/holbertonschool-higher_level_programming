9d835185a738bffbdc602ac7c2bcba55cf435645
Katherine Beringer <github@katbee.au>
2025-09-14 12:34:13 +1000

    Improve documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -19,8 +19,9 @@ def text_indentation(text):
     markers = ['.', '?', ':', '*** ']
     text_copy = text[:]
 
-    #   Create a placeholder string with standardised delimiters
-    #   Remove space at sentence boundaries
+    # Create a placeholder string with standardised delimiters
+    #   The original delimiter from the print string must be retained
+    #   Spaces at sentence boundaries must be skipped
     for marker in markers:
         if marker != '*** ':
             delimiter = marker + '***'
@@ -38,4 +39,13 @@ def text_indentation(text):
         print("{}".format(ch), end='\n\n')
 
 # text = "string to print. second string? third string: fourth string?"
+# text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+# Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+# Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+# Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+# Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+# rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+# stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+# cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+# beatiorem! Iam ruinas videres"""
 # text_indentation(text)
