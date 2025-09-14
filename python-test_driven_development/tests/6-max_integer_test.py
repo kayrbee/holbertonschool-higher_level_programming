a6e3230681dc1c9934a819641d488f74efd43e63
Katherine Beringer <github@katbee.au>
2025-09-14 13:35:57 +1000

    Fix test input for max_at_beginning

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,10 +6,10 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_at_the_end(self):
-        self.assertEquals(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
     
     def test_max_at_the_beginning(self):
-        self.assertEquals(max_integer([1, 2, 3, 4]), 1)
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
 
     def test_empty_list(self):
         self.assertIsNone(max_integer([]))
