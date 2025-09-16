77a10d842ab27ab09d7ec83948b88a165621653f
Katherine Beringer <github@katbee.au>
2025-09-16 11:16:36 +1000

    Fix/typo in error message

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -91,5 +91,5 @@ class Square:
         # self.__position = value
         if (not isinstance(value, tuple) or len(value) != 2
              or not all(isinstance(num, int) and num >= 0 for num in value)):
-            raise TypeError("Position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
