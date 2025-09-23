ea62e90eaa85890b902d621aea87a241c8ea6477
Katherine Beringer <github@katbee.au>
2025-09-23 14:26:35 +1000

    Implement area and str in subclass

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module contains one subclass
+Inherits from 7-base_geometry.py
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle subclass of BaseGeometry
+    Has width and height
+    """
+    def __init__(self, width, height):
+        """
+        Initialise with width and height properties
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
