f2f7388b25f0ef6284407bb93cab5b7b72ff1774
Katherine Beringer <github@katbee.au>
2025-09-23 14:39:44 +1000

    Tidy up Square class for checker

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -16,13 +16,7 @@ class Square(Rectangle):
         Initialise with width and height from size
         """
         super().__init__(size, size)
-        # Rectangle.__init__(self, size, size)
         self.__size = size
-        # super().integer_validator("size", size)
-        # self.__width = size
-        # self.__height = size
 
     def area(self):
         return self.__size * self.__size
-    # def __str__(self):
-    #     return ("[Rectangle] {}/{}".format(self.__width, self.__height))
