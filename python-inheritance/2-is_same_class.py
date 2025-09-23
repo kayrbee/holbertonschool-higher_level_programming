3af77bfda66b3a64883a50f533a95b8f9c875b3b
Katherine Beringer <github@katbee.au>
2025-09-23 11:46:20 +1000

    Fix pycodestyle

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,9 +3,10 @@
 Module contains one function
 """
 
+
 def is_same_class(obj, a_class):
     """
     Function checks object is of type
     Returns boolean
     """
-    return (True if type(obj) == a_class else False)
+    return (True if type(obj) is a_class else False)
