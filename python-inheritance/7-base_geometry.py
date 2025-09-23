4de7a3118fed73bc221c8740600603e8a2eae535
Katherine Beringer <github@katbee.au>
2025-09-23 12:36:04 +1000

    Implement tests then class methods

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module contains one class
+Builds on 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    Implements 2 public instance methods
+        def area(self):
+            raises an Exception with the message
+            "area() is not implemented"
+        def integer_validator(self, name, value):
+            you can assume name is always a string
+            if value is not an integer: raise a TypeError exception
+              with the message "<name> must be an integer"
+            if value is less or equal to 0: raise a ValueError
+              exception with the message "<name> must be greater than 0"
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            s = name + " must be an integer"
+            raise TypeError(s)
+        if value <= 0:
+            s = name + " must be greater than 0"
+            raise ValueError(s)
