93a529ce84b17012f478dcc8015561eff19224ff
Katherine Beringer <github@katbee.au>
2025-09-25 12:08:55 +1000

    Duck typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Module contains abstract class and
+  related subclasses
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    """
+    Abstract class - Shape
+    """
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """
+    Class inherits from Shape
+    """
+    pi = 3.141592653589793
+
+    def __init__(self, radius):
+        self.__radius = radius
+    
+    def area(self):
+        return (self.__class__.pi * self.__radius ** 2)
+    
+    def perimeter(self):
+        return (2 * self.__class__.pi * self.__radius)
+
+
+
+class Rectangle(Shape):
+    """
+    Class inherits from Shape
+    """
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        return (self.__width * self.__height)
+    
+    def perimeter(self):
+        return (2 * self.__width + 2 * self.__height)
+
+def shape_info(self):
+    area = self.area()
+    perimeter = self.perimeter()
+    print("Area: {}".format(area))
+    print("Perimeter: {}".format(perimeter))
