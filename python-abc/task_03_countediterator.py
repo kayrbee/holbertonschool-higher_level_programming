8704edc3a66fecd48e031804ebed7842ae04f051
Katherine Beringer <github@katbee.au>
2025-09-25 15:21:59 +1000

    Empty class

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module contains a class which
+  extends iter()
+"""
+
+
+class CountedIterator:
+    """
+    CountedIterator extends iter()
+    function to track the number of items
+    iterated over.
+
+    Overrides __next__() with a counter
+    """
+    pass
