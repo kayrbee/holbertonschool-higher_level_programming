0b2f7fc3c9d54f46063e3b240324547d7025795f
Katherine Beringer <github@katbee.au>
2025-09-25 16:10:02 +1000

    Set up classes for multiple inheritance

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module contains a class which
+  uses multiple inheritance
+"""
+
+
+class Fish:
+    def swim(self):
+        print("The fish is swimming")
+    
+    def habitat(self):
+        print("The fish lives in water")
+
+class Bird:
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+class FlyingFish(Fish, Bird):
+    pass
