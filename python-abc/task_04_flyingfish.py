1b95d6c35f2d9778bd7c65e2c534d52ca6390136
Katherine Beringer <github@katbee.au>
2025-09-25 16:20:25 +1000

    Play with MRO

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -20,4 +20,12 @@ class Bird:
         print("The bird lives in the sky")
 
 class FlyingFish(Fish, Bird):
-    pass
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        # super().habitat()
+        print("The flying fish lives both in water and the sky!")
