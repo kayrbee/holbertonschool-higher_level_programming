96c41dd695e405982db8b4980cf4ebd3477bdc5a
Katherine Beringer <github@katbee.au>
2025-09-30 15:16:56 +1000

    Fix/add file_exists? check

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,12 +6,17 @@ a Python list, and then save them to a file
 
 
 import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
-my_list = load_from_json_file(filename)
+if os.path.exists("./" + filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
 length = len(sys.argv)
 
 with open(filename, "a", encoding="utf-8"):
