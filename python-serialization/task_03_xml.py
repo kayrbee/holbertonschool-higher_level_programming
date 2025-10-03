4a1d63ad2225d78b9c1639c0dc8430085ee01f8f
Katherine Beringer <github@katbee.au>
2025-10-03 13:13:46 +1000

    First draft xml conversion

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Use xml module to convert to/from xml
+"""
+
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """
+    Convert a dict object to xml file
+    """
+    if not dictionary or not filename:
+        print(f"[XML Conversion] dict or filename not provided")
+        return False
+
+    try:
+        root = ET.Element("root")
+
+        for key, value in dictionary.items():
+            child = ET.SubElement(root, key)
+            child.text = str(value)
+
+        tree = ET.ElementTree(root)
+        tree.write(filename, encoding="utf-8", xml_declaration=True)
+    except (ET.ParseError, TypeError, AttributeError) as e:
+        print(e)
+    except FileNotFoundError as e:
+        pass
+    
+def deserialize_from_xml(filename):
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        data = {}
+        for child in root:
+            data[child.tag] = child.text
+        data["age"] = int(data["age"])
+        return data
+    except Exception as e:
+        print(f"Deserialisation error {e}")
