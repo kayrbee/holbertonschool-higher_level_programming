c1abe8a65cb0dc18e09136132b411eedff3c54e6
Katherine Beringer <github@katbee.au>
2025-10-03 12:19:10 +1000

    Add basic error handling

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -12,15 +12,24 @@ def convert_csv_to_json(csv_filename):
     """
     Convert a csv file to a json file
     """
-    with open(csv_filename, "r", encoding="utf-8") as csvf:
-        csvreader = csv.DictReader(csvf)  # Create DictReader
+    if not csv_filename:
+        print(f"[CSV Conversion] CSV file not provided")
+        return False
 
-        data_list = []  # Store dictionaries in list
-        for row in csvreader:  # Read rows into dicts
-            data_list.append(row)
-        print("Data list: ", data_list)
+    try:
+        with open(csv_filename, "r", encoding="utf-8") as csvf:
+            csvreader = csv.DictReader(csvf)  # Create DictReader
 
-    with open("data.json", "w", encoding="utf-8") as jf:
-        # for item in data_list:
-        #     json.dump(data_list, jf)
-        json.dump(data_list, jf)
+            data_list = []  # Store dictionaries in list
+            for row in csvreader:  # Read rows into dicts
+                data_list.append(row)
+            print("Data list: ", data_list)
+
+        with open("data.json", "w", encoding="utf-8") as jf:
+            # for item in data_list:
+            #     json.dump(data_list, jf)
+            json.dump(data_list, jf)
+        return True
+    except Exception as e:
+        print(f"[CSV Conversion] Exception occurred: {e}")
+        return False
