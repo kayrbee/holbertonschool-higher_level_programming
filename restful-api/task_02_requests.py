961ac3f09220cc59cbbb1119d3d236361c010cb2
Katherine Beringer <github@katbee.au>
2025-10-07 16:17:53 +1100

    Wrap logic in fetch_and_print_posts() function

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,10 +4,11 @@ import requests
 
 
 # Fetch posts from JSONPlaceholder
-posts = requests.get('https://jsonplaceholder.typicode.com/posts')
-print("Status code: {}".format(posts.status_code))
-if posts.status_code >= 200 and posts.status_code < 300:
-    posts_json = posts.json()
-    for post in posts_json:
-        print(post["title"])
+def fetch_and_print_posts():
+    posts = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print("Status code: {}".format(posts.status_code))
+    if posts.status_code >= 200 and posts.status_code < 300:
+        posts_json = posts.json()
+        for post in posts_json:
+            print(post["title"])
 # Fetch and save posts to csv file
