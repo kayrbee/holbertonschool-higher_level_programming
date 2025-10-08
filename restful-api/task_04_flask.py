1762a437756fb240a60a5bd1b220e6cf75612195
Katherine Beringer <github@katbee.au>
2025-10-08 14:34:35 +1100

    Implement /data route

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
+@app.route("/data")
+def data():
+    names = []
+    for user in users:
+        name = user["name"]
+        names.append(name)
+    return jsonify(names)
+
 # Run server
-if __name__ == "__main__": app.run()
+if __name__ == "__main__": 
+    app.run()
