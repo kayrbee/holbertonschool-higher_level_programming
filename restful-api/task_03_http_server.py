a90c54ac7535ea8d82f637faffd0ce3d991c0363
Katherine Beringer <github@katbee.au>
2025-10-08 13:04:09 +1100

    Extend do_GET() to handle multiple paths and 404

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 import http.server
 import socketserver
+import json
 
 
 PORT = 8000
+data = {"name": "John", "age": 30, "city": "New York"}
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello, world!")  # NB: must be binary string
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")  # NB: must be binary string
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            json_string = json.dumps(data)
+            self.wfile.write(json_string.encode("utf-8"))  # NB: must be binary string
+        else:
+            self.send_error(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Page Not Found")  # NB: must be binary string
 
 # Start the server
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
