a6ed5b6770ac99346d5bf967f69274454fc005ab
Katherine Beringer <github@katbee.au>
2025-10-08 14:09:58 +1100

    Set up flask and add first endpoint

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from flask import Flask
-import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "Welcome to the Flask API!"
+
+# Run server
+if __name__ == "__main__": app.run()
