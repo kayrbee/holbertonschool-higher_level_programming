56533e7d749d88ae2cb2ec73bbf0f5091f0740fd
Katherine Beringer <github@katbee.au>
2025-10-08 15:26:15 +1100

    Explicitly set the return order on /users/user

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,24 +6,53 @@ from flask import request
 app = Flask(__name__)
 
 users = {}
+# For testing
+users = {
+    "jane":
+    {"username": "jane",
+     "name": "Jane",
+     "age": 28,
+     "city": "Los Angeles"
+     },
+    "john":
+    {"username": "john",
+     "name": "John",
+     "age": 30,
+     "city": "New York"
+     }
+}
+
 
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route("/data")
 def data():
     usernames = list(users.keys())
     return jsonify(usernames)
 
+
 @app.route("/status")
 def status():
     return "OK"
 
+
 @app.route("/users/<username>")
 def user(username):
-    user = users[username]
-    return jsonify(user)
+    if username in users.keys():
+        return jsonify({
+            "username": users[username]["username"],
+            "name": users[username]["name"],
+            "age": users[username]["age"],
+            "city": users[username]["city"]
+        })
+    else:
+        error = {"error": "User not found"}
+        return jsonify(error), 404
+
+
 # Run server
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run()
