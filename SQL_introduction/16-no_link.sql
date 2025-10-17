c3bb4284af9124d18358ab43f80ac1d0ac78a2f7
Katherine Beringer <github@katbee.au>
2025-10-17 12:39:54 +1100

    Select rows where value is not null

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -3,3 +3,6 @@
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
 -- The database name will be passed as an argument to the mysql command
+SELECT score, name FROM second_table
+WHERE name IS NOT null
+ORDER BY score DESC;
