7fcfb981aeb28e34e2028653b0aa4225f9b41375
Katherine Beringer <github@katbee.au>
2025-10-26 17:57:42 +1100

    Fix indentation of if __name__ block

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,27 +7,27 @@ You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
 Results must be displayed as they are in the example below
-Your code should not be executed when imported
 """
 if __name__ == '__main__':
+    """ Your code should not be executed when imported """
     import MySQLdb
     import sys
-# Get argv inputs
-arguments = sys.argv
-user = arguments[1]
-password = arguments[2]
-database = arguments[3]
+    # Get argv inputs
+    arguments = sys.argv
+    user = arguments[1]
+    password = arguments[2]
+    database = arguments[3]
 
-# Connect to db
-db = MySQLdb.connect(host='localhost', user=user,
-                     passwd=password, database=database, port=3306)
-cur = db.cursor()
+    # Connect to db
+    db = MySQLdb.connect(host='localhost', user=user,
+                         passwd=password, database=database, port=3306)
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states SORT BY states.id ASC")  # Execute query
-# Fetch results and print them. Rows are returned as tuples by default
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-# Close the cursor and connection
-cur.close()
-db.close()
+    cur.execute("SELECT * FROM states SORT BY states.id ASC")  # Execute query
+    # Fetch results and print them. Rows are returned as tuples by default
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Close the cursor and connection
+    cur.close()
+    db.close()
