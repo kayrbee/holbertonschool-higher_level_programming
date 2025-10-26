1070c94b26f5c54f382999bdf7a840177738bb68
Katherine Beringer <github@katbee.au>
2025-10-26 23:49:02 +1100

    Insert record

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Write a script that prints the State object with the
+ name passed as argument from the database hbtn_0e_6_usa
+
+Your script should take 4 arguments: mysql username,
+ mysql password, database name and state name to search (SQL injection free)
+You must use the module SQLAlchemy
+You must import State and Base from model_state
+ - from model_state import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+You can assume you have one record with the state name to search
+Results must display the states.id
+If no state has the name you searched for, display Not found
+Your code should not be executed when imported
+"""
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{passwd}@localhost:3306/{db_name}"
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name=state_name)
+
+    state = session.add(new_state)
+    session.commit()
+
+    print(f"{state.id}")
+
+    session.close()
