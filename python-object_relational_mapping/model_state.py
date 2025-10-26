f892b7f13f5ed462ee92dcd21abed9ac9a9d74df
Katherine Beringer <github@katbee.au>
2025-10-26 22:55:17 +1100

    Create a State class with SQLAlchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Write a python file that contains the class definition of a State
+and an instance Base = declarative_base():
+
+State class:
+  inherits from Base Tips
+  links to the MySQL table states
+  class attribute id that represents a column of an auto-generated,
+  unique integer, can’t be null and is a primary key
+  class attribute name that represents a column of a string with
+  maximum 128 characters and can’t be null
+You must use the module SQLAlchemy
+Your script should connect to a MySQL server running on localhost at port 3306
+WARNING: all classes who inherit from Base must be imported
+before calling Base.metadata.create_all(engine)
+"""
+if __name__ == __main__:
+    """ Don't run on import"""
+    from sqlalchemy import create_engine, Column, Integer, String
+    from sqlalchemy.ext.declarative import declarative_base
+
+    Base = declarative_base()
+
+    class State(Base):
+        __tablename__ = "states"
+
+        id = Column(Integer, primary_key=True,
+                    autoincrement=True, nullable=False)
+        name = Column(String(128), nullable=False)
+
+        def __repr__(self):
+            return f"<State(id={self.id}, name='{self.name}')>"
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
