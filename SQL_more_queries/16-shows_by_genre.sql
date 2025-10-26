3bc1a63f5513f0dd9971d4505ab1f9b35674b776
Katherine Beringer <github@katbee.au>
2025-10-26 17:06:14 +1100

    Fix/AS keyword in caps

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title AS title, tv_genres.name as `name`
+SELECT tv_shows.title AS title, tv_genres.name AS `name`
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
