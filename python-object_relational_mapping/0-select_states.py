9e3e34cc870cb033d8ee2dfb45c86ba650f07bfb
Katherine Beringer <github@katbee.au>
2025-10-26 17:45:52 +1100

    Use MySQLdb to execute a query

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa:
+
+Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+# Get argv inputs
+arguments = sys.argv
+user = arguments[1]
+password = arguments[2]
+database = arguments[3]
+
+# Connect to db
+db = MySQLdb.connect(host='localhost', user=user,
+                     passwd=password, database=database, port=3306)
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states SORT BY states.id ASC")  # Fetch records
+rows = cur.fetchall()
+for row in rows:
+    for col in row:
+        print("%s," % col)
+    print("\n")
