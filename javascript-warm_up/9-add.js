acc1de29795b424f472149ad4a7542adb126100d
Katherine Beringer <github@katbee.au>
2025-11-02 21:05:31 +1100

    add(a, b)

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const a = Number(args[2]);
+const b = Number(args[3]);
+const myErr = 'NaN';
+
+if (args.length < 3 || isNaN(a) || isNaN(b)) {
+  console.log(myErr);
+  process.exit(0);
+}
+
+function add(a, b) {
+  return (a + b);
+}
+
+console.log(add(a, b));
