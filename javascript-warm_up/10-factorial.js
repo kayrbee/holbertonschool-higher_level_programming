9e0726a0cdb3240ded1932a9b20b6370b1765700
Katherine Beringer <github@katbee.au>
2025-11-02 21:28:57 +1100

    Move NaN check to function

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,15 +2,9 @@
 const process = require('process');
 const args = process.argv;
 const a = Number(args[2]);
-const myErr = 'NaN';
-
-if (args.length < 3 || isNaN(a)) {
-  console.log(myErr);
-  process.exit(0);
-}
 
 function factorial (a) {
-  if (a === 0 || a === 1) {
+  if (a === 0 || a === 1 || isNaN(a)) {
     return 1;
   }
   return (a * factorial(a - 1));
