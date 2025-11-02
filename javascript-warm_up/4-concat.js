8a19548b3c4990bf1239bd0da2339aa8c646d38c
Katherine Beringer <github@katbee.au>
2025-11-02 12:19:07 +1100

    Print two arguments with a spacer string

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('node:process');
+const myArgs = process.argv
+const numArgs = +myArgs.length;
+const undef = 'undefined'
+const spacerString = 'is'
+
+if (numArgs === 2) {
+  console.log(undef, spacerString, undef);
+} else if (numArgs === 3) {
+  console.log(process.argv[2], spacerString, undef);
+} else {
+  console.log(process.argv[2], spacerString, process.argv[3]);
+}
