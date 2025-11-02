a44dbb151c4a1cf2d43dcee69d9e934d2cbf094f
Katherine Beringer <github@katbee.au>
2025-11-02 11:54:56 +1100

    Fix/semistandard validations

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,10 +3,10 @@ const process = require('process');
 const myArgs = process.argv;
 const numArgs = +myArgs.length;
 
-if (numArgs == 2) {
-    console.log('No argument');
-} else if (numArgs == 3) {
-    console.log('Argument found');
+if (numArgs === 2) {
+  console.log('No argument');
+} else if (numArgs === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
