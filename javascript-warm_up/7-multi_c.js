96ba578a13c4ba4e95404748421a5d15a6198f25
Katherine Beringer <github@katbee.au>
2025-11-02 13:08:29 +1100

    Refactor to use exactly 2 prints

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
+const numIterations = Number(args[2]);
 const myString = 'C is fun';
 const myErr = 'Missing number of occurrences';
 let i = 0;
 
-if (args.length === 2) {
+if (args.length === 2 || isNaN(numIterations)) {
   console.log(myErr);
 } else {
   const numIterations = Number(args[2]);
-  if (Number.isInteger(numIterations)) {
-    while (i < numIterations) {
-      console.log(myString);
-      i++;
-    }
-  } else {
-    console.log(myErr);
+  while (i < numIterations) {
+    console.log(myString);
+    i++;
   }
 }
