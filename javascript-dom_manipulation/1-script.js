aaa399eca0ac021c89330131630528e6c5604a2c
Katherine Beringer <github@katbee.au>
2025-11-03 11:05:19 +1100

    Add comment for alternative solution

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -2,9 +2,15 @@
 const header = document.querySelector('header');
 const target = document.getElementById('red_header');
 
-// const click = target.addEventListener(MouseEvent("click"));
-const click = target.addEventListener("click", changeColor);
-
 function changeColor() {
     header.style.color = '#FF0000';
 }
+
+target.addEventListener("click", changeColor);
+
+
+// Alternative approach 
+// - inline anonymous function
+// Produces equivalent behaviour to original implementation
+// Good for cases where the handler is short and won't be reused
+// target.addEventListener("click", () => { header.style.color = '#FF0000'; });
