7c431c7612d5cab4a2ee60f35a357514f8f28589
Katherine Beringer <github@katbee.au>
2025-11-14 10:11:27 +1100

    toggle the value of an attribute on click

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const header = document.querySelector('header');
+const target = document.getElementById('toggle_header');
+
+function updateClassValue() {
+    if (header.getAttribute("class") == "red") {
+        header.setAttribute("class", "green");
+    }
+    else {
+        header.setAttribute("class", "red");
+    }
+}
+
+target.addEventListener("click", updateClassValue);
