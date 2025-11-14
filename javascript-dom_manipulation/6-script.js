c62ba5961151daab2f3d61c715447eff92389570
Katherine Beringer <github@katbee.au>
2025-11-14 11:33:39 +1100

    used fetch() to return json data

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+async function get_char_name() {
+    try {
+        const response = await fetch('https://swapi-api.hbtn.io/api/people/5/?format=json');
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const character = await response.json();
+        return (character['name']);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+// Version 2 solution
+document.getElementById('character').innerHTML = await get_char_name();
+
+// Version 1 solution
+// const char_name = await get_char_name();
+// const character = document.getElementById('character');
+// character.innerHTML = char_name;
