f19322c26a79e276b21004817723c1360c7e9276
Katherine Beringer <github@katbee.au>
2025-11-14 11:02:14 +1100

    Refactored to add li on every click

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const list_item = document.createElement("li");
-const node = document.createTextNode("Item")
-list_item.appendChild(node);
 
 const list = document.querySelector('.my_list')
 const target = document.getElementById('add_item');
 
 function addListItem() {
+    const list_item = document.createElement("li");
+    const node = document.createTextNode("Item")
+    list_item.appendChild(node);
     list.appendChild(list_item)
 }
 
