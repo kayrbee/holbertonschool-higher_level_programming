c30f2b6ef3de5f61e0bf5173ea82052c5dd3c909
Katherine Beringer <github@katbee.au>
2025-11-14 12:12:50 +1100

    Step 3 - loop over array of movie titles

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -24,17 +24,12 @@ for (i = 0; i < num_movies; i++) {
     movie_titles.push(movie_title);
 }
 
-// Create new li element for first movie
-const list_item = document.createElement('li');
-const node = document.createTextNode(movie_titles[0]);
-list_item.appendChild(node);
+// Loop over array of movie titles to add each movie to a li element
+for (i = 0; i < num_movies; i++) {
+    const list_item = document.createElement('li');
+    const node = document.createTextNode(movie_titles[i]);
+    list_item.appendChild(node);
 
-// Find the list element
-const list = document.getElementById('list_movies');
-
-// Add movie title to list
-list.appendChild(list_item);
-
-// function addMovieTitle() {
-
-// }
+    const list = document.getElementById('list_movies');
+    list.appendChild(list_item);
+}
