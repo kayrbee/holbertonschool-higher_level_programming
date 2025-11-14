62bed21a00414493c3467183d69b7fdf4acb4e47
Katherine Beringer <github@katbee.au>
2025-11-14 12:07:23 +1100

    Step 2 - add first movie title to list

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -15,9 +15,26 @@ async function get_movies() {
 }
 movies = await get_movies();
 num_movies = movies['count'];
+movie_titles = [];
 
+// Create an array of titles
 for (i = 0; i < num_movies; i++) {
     movie = movies['results'][i];
     movie_title = movie['title'];
-    console.log(movie_title);
+    movie_titles.push(movie_title);
 }
+
+// Create new li element for first movie
+const list_item = document.createElement('li');
+const node = document.createTextNode(movie_titles[0]);
+list_item.appendChild(node);
+
+// Find the list element
+const list = document.getElementById('list_movies');
+
+// Add movie title to list
+list.appendChild(list_item);
+
+// function addMovieTitle() {
+
+// }
