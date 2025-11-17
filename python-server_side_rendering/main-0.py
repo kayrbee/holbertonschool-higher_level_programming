3d1f9073db0227bd5b0fc772f05ff93810018a7b
Katherine Beringer <github@katbee.au>
2025-11-17 13:16:43 +1100

    Handle missing keys

--- python-server_side_rendering/main-0.py
+++ python-server_side_rendering/main-0.py
@@ -8,6 +8,16 @@ with open('template.txt', 'r') as file:
 # Test empty template
 # template_content = None
 
+# # List of attendees
+# attendees = [
+#     {"name": "Alice", "event_title": "Python Conference",
+#         "event_date": "2023-07-15", "event_location": "New York"},
+#     {"name": "Bob", "event_title": "Data Science Workshop",
+#         "event_date": "2023-08-20", "event_location": "San Francisco"},
+#     {"name": "Charlie", "event_title": "AI Summit",
+#         "event_date": None, "event_location": "Boston"}
+# ]
+
 # List of attendees
 attendees = [
     {"name": "Alice", "event_title": "Python Conference",
@@ -15,7 +25,7 @@ attendees = [
     {"name": "Bob", "event_title": "Data Science Workshop",
         "event_date": "2023-08-20", "event_location": "San Francisco"},
     {"name": "Charlie", "event_title": "AI Summit",
-        "event_date": None, "event_location": "Boston"}
+        "event_location": "Boston"}
 ]
 
 # Call the function with the template and attendees list
--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -20,14 +20,20 @@ def generate_invitations(template: str, attendees):
         if os.path.exists(output):
             continue
 
+        # Handle empty values in key
         for key in attendee:
             if not attendee[key]:
                 attendee[key] = f'{key}: N/A'
 
+        # Handle missing keys
+        event_keys = ['name', 'event_title', 'event_date', 'event_location']
+        for key in event_keys:
+            attendee.setdefault(key, f'{key}: N/A')
+
         # Get attendee details
         name = attendee['name']
         event_title = attendee['event_title']
-        event_date = attendee['event_date'] or "event_date: N/A"
+        event_date = attendee['event_date']
         event_location = attendee['event_location']
 
         try:
