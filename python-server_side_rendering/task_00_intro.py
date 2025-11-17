a6eb1db742c6e5b6f17379ba9fd24a7fe4fd1a92
Katherine Beringer <github@katbee.au>
2025-11-17 12:15:05 +1100

    Use exit() for empty variables

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -3,9 +3,9 @@ import os
 
 def generate_invitations(template: str, attendees):
     if not template:
-        raise ValueError("Template is empty, no output files generated")
+        exit("Template is empty, no output files generated")
     if not attendees:
-        raise ValueError("No data provided, no output files generated")
+        exit("No data provided, no output files generated")
     if not isinstance(template, str):
         raise TypeError("Template must be string")
     if not isinstance(attendees, list):
